b8e35c8332d2d725b4657d58b8bfc5e51e9df52c
michael mason <michael@michaeljmason.com>
2017-07-14 15:52:11 -0700

    api

--- assets/js/script.js
+++ assets/js/script.js
@@ -8,6 +8,43 @@ var selectedChoice;
 var numberOfAnswers = 0;
 var index;
 
+
+// var dataURL = 'https://opentdb.com/api.php?amount=10&category=18&difficulty=medium&type=multiple';
+
+// var promise = new Promise((done, fail) => {
+// 	var trivia = [];
+// 	$.getJSON(dataURL, function(data) {
+// 		$.each(data, function(key, val) {
+// 			trivia.push({key, val});
+// 		});
+// 		done(trivia);
+// 	});
+// });
+
+// promise.then(arr => {
+// 	let trivia = arr[1].val;
+// 	for(let object in trivia) {
+// 		console.log(trivia[object].question);
+// 	}
+// 	let question = trivia[0].question;
+// 	let answers = trivia[0].incorrect_answers;
+// 	let correct_answer = trivia[0].correct_answer;
+// 	ask(question, answers, correct_answer);
+// });
+
+// function ask(question, incorrect_answers, correct_answer) {
+// 	$('#Question').append(question);
+// 	let answers = [correct_answer];
+// 	for(let answer in incorrect_answers) {
+// 		answers.push(incorrect_answers[answer]);
+// 	}
+// 	answers.sort();
+// 	for(let answer in answers) {
+// 		$('#Answers form').append(`<input type="radio" value=" ${answers[answer]} "> ${answers[answer]} <br>`);
+// 	}
+// }
+
+
 var test = {
 	score: 0,
 	time: 60,
