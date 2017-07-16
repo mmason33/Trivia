6f793d65325749aec5e2e24ec95439aa6de925d7
Michael Mason <michael@michaeljmason.com>
2017-07-16 01:55:45 -0700

    i think im done

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,18 +1,17 @@
-var pickedCat;
-var questionCounter = 0;
-var correctAnswers = 0;
-var incorrectAnswers = 0;
-var unansweredQuestions = 0;
-var timer;
-var selectedChoice;
-var numberOfAnswers = 0;
-var index;
-var choiceAsNumber;
 
 var trivia = {
 	score: 0,
-	time: 60,
-	directions: 'Select a category. Each question is worth $100,000. There are 10 questions and you have 60 seconds to answer each question. Good luck and hopefully you become a Millionaire!!',
+	time: 3,
+	utils: {
+		selectedIndex: 0,
+		questionCounter: 0,
+		questionNumber: 1,
+		correctAnswers: 0,
+		incorrectAnswers: 0,
+		unansweredQuestions: 0,
+		numberOfAnswers: 0,
+		directions: 'Select a category. Each question is worth $100,000. There are 10 questions and you have 60 seconds to answer each question. Good luck and hopefully you become a Millionaire!!',
+	},
 	categories: [
 		{
 			name: 'Math',
@@ -80,7 +79,7 @@ var trivia = {
 			]
 		},
 		{
-			name: 'Sports',
+			name: 'Football',
 			questions: [
 				{
 					amount: '100,000',
@@ -90,122 +89,57 @@ var trivia = {
 				},
 				{
 					amount: '200,000',
-					theQuestion: '',
-					choices: [],
-					answer: 1
+					theQuestion: 'How many NBA teams are in California?',
+					choices: ['2', '3', '4', '5'],
+					answer: 2
 				},
 				{
 					amount: '300,000',
-					theQuestion: '',
-					choices: [],
-					answer: 1
+					theQuestion: 'What team does Lebron James play for?',
+					choices: ['Kings', 'Lakers', 'Mavericks', 'Cavaliers'],
+					answer: 3
 				},
 				{
 					amount: '400,000',
-					theQuestion: '',
-					choices: [],
-					answer: 1
+					theQuestion: 'How many NFL teams are in Texas?',
+					choices: ['2', '3', '4', '5'],
+					answer: 0
 				},
 				{
 					amount: '500,000',
-					theQuestion: '',
-					choices: [],
-					answer: 1
+					theQuestion: 'Where did Peyton Manning go to college?',
+					choices: ['Stanford', 'USC', 'SMU', 'Tennessee'],
+					answer: 3
 				},
 				{
 					amount: '600,000',
-					theQuestion: '',
-					choices: [],
+					theQuestion: 'How many NFL teams have never won a superbowl?',
+					choices: ['12', '13', '14', '15'],
 					answer: 1
 				},
 				{
 					amount: '700,000',
-					theQuestion: '',
-					choices: [],
-					answer: 1
+					theQuestion: 'Where did Marshawn Lynch go to college?',
+					choices: ['Texas', 'Alabama', 'Auburn', 'Berkley'],
+					answer: 3
 				},
 				{
 					amount: '800,000',
-					theQuestion: '',
-					choices: [],
-					answer: 1
+					theQuestion: 'What quarterback has the most superbowls?',
+					choices: ['Troy Aikman', 'Jim Kelly', 'Tom Brady', 'Aaron Rogers'],
+					answer: 2
 				},
 				{
 					amount: '900,000',
-					theQuestion: '',
-					choices: [],
-					answer: 1
+					theQuestion: 'How long is a football field?',
+					choices: ['100 yards', '110 yards', '120 yards', '130 yards'],
+					answer: 2
 				},				
 				{
 					amount: '1,000,000',
-					theQuestion: '',
-					choices: [],
-					answer: 1
-				}								
-			]
-		},
-		{
-			name: 'Computers',
-			questions: [
-				{
-					amount: '100,000',
-					theQuestion: '',
-					choices: [],
-					answer: 1
-				},
-				{
-					amount: '200,000',
-					theQuestion: '',
-					choices: [],
-					answer: 1
-				},
-				{
-					amount: '300,000',
-					theQuestion: '',
-					choices: [],
-					answer: 1
-				},
-				{
-					amount: '400,000',
-					theQuestion: '',
-					choices: [],
-					answer: 1
-				},
-				{
-					amount: '500,000',
-					theQuestion: '',
-					choices: [],
-					answer: 1
-				},
-				{
-					amount: '600,000',
-					theQuestion: '',
-					choices: [],
-					answer: 1
-				},
-				{
-					amount: '700,000',
-					theQuestion: '',
-					choices: [],
-					answer: 1
-				},
-				{
-					amount: '800,000',
-					theQuestion: '',
-					choices: [],
-					answer: 1
-				},
-				{
-					amount: '900,000',
-					theQuestion: '',
-					choices: [],
-					answer: 1
-				},				
-				{
-					amount: '1,000,000',
-					theQuestion: '',
-					choices: [],
-					answer: 1
+					theQuestion: 'What year was the first NFL game?',
+					choices: ['1917', '1936', '1942', '1920'],
+					answer: 3
 				}								
 			]
 		}
@@ -213,110 +147,224 @@ var trivia = {
 	],
 
 	categoryClick: function (event) {
+
 		for (let cat in this.categories) {
-			if ($(event.target).attr('id') === trivia.categories[cat].name) {
-				$('.header').append('<h1 class="cat-title animated fadeIn">' + trivia.categories[cat].name + '</h1>');
-				pickedCat = $(event.target).attr('id');
-				index = cat;
+
+			if ($(event.currentTarget).attr('id') === trivia.categories[cat].name) {
+
+				$('.header').append(
+					'<h1 class="cat-title animated fadeIn">' + trivia.categories[cat].name + '</h1>'
+				);
+
+				this.utils.selectedIndex = cat;
+
 			}
+
 		}
 		this.question();
 	},
+
 	question: function () {
-		var questionNumber = questionCounter + 1;
 		$('.trivia-wrap').empty();
-		param =	$('.trivia-wrap').append('<h3 class="animated fadeIn">'+ questionNumber + '. ' + this.categories[index].questions[questionCounter].theQuestion + '</h3>');
-		this.questionValue();
+		param =	$('.trivia-wrap').append(
+			'<h3 class="animated fadeIn">'+ this.utils.questionNumber + '. ' + this.categories[this.utils.selectedIndex].questions[trivia.utils.questionCounter].theQuestion + '</h3>' +
+			'<h4 class="animated fadeIn">' + 'For: ' + '$' + this.categories[this.utils.selectedIndex].questions[trivia.utils.questionCounter].amount + '</h4>'
+		);
 		this.avalChoices();
 		this.timer();
-		console.log(this.categories[index].questions[questionCounter].choices[this.categories[index].questions[questionCounter].answer]);
-	},
-	questionValue: function () {
-		$('.trivia-wrap').append('<h4 class="animated fadeIn">' + 'For: ' + '$' + this.categories[index].questions[questionCounter].amount + '</h4>');
+		this.utils.questionNumber++;
+
+		console.log(
+			'Correct Answer', this.categories[this.utils.selectedIndex].questions[trivia.utils.questionCounter].choices[this.categories[this.utils.selectedIndex].questions[trivia.utils.questionCounter].answer]
+		);
+
 	},
 
 	avalChoices: function () {
+
 		$('.trivia-wrap').append(
-			'<div class="row justify-content-center animated fadeIn"><div class="col-5 text-center">' +
-			'<button id="0"class="choice">' + 'A. ' + this.categories[index].questions[questionCounter].choices[0] + '</button>' +
-			'<button id="1"class="choice">' + 'B. ' + this.categories[index].questions[questionCounter].choices[1] + '</button>' +
-			'<button id="2"class="choice">' + 'C. ' + this.categories[index].questions[questionCounter].choices[2] + '</button>' +
-			'<button id="3"class="choice">' + 'D. ' + this.categories[index].questions[questionCounter].choices[3] + '</button>' +
-			'</div></div>'
-		);	
-		$('.choice').click(trivia.answerClick);
+			'<div class="row justify-content-center animated fadeIn">' +
+				'<div class="col-5 text-center">' +
+					'<button id="0"class="choice">' + 'A. ' + this.categories[this.utils.selectedIndex].questions[trivia.utils.questionCounter].choices[0] + '</button>' +
+					'<button id="1"class="choice">' + 'B. ' + this.categories[this.utils.selectedIndex].questions[trivia.utils.questionCounter].choices[1] + '</button>' +
+					'<button id="2"class="choice">' + 'C. ' + this.categories[this.utils.selectedIndex].questions[trivia.utils.questionCounter].choices[2] + '</button>' +
+					'<button id="3"class="choice">' + 'D. ' + this.categories[this.utils.selectedIndex].questions[trivia.utils.questionCounter].choices[3] + '</button>' +
+				'</div>' +
+			'</div>'
+		);
+
 	},
 
 	timer: function () {
+
 		$('.time-wrap').removeClass('hide');
 		$('.timer').text(this.time);
 		var currentTime = this.time;
-		timer =	setInterval(function () {
+
+		var timer =	setInterval( function () {
+
 			currentTime--;
 			$('.timer').text(currentTime);
 
-			if (currentTime === 0 && questionCounter !== 9) {
-				questionCounter++;
-				trivia.evalAnswer();
+			if (currentTime === 0 && trivia.utils.questionCounter !== 9) {
+
+				trivia.utils.questionCounter++;
+				trivia.evalAnswerChosen();
 				trivia.question();
+
 			}
 
 		}, 1000);
 
+		$('.choice').click( function (event) {
+			trivia.answerClick(timer, event.currentTarget);
+		});
+
 	},
 
-	answerClick: function () {
-		selectedChoice = $(this).attr('id');
-		choiceAsNumber = parseInt(selectedChoice, 10);
-		console.log(choiceAsNumber, trivia.categories[index].questions[questionCounter].answer)
-		clearInterval(timer);
-		if (numberOfAnswers < 9) {
-			trivia.evalAnswer();
-		 	questionCounter++;
-		 	numberOfAnswers++;
+	answerClick: function (interval, event) {
+
+		var selectedChoice = $(event).attr('id');
+		var choiceAsNumber = parseInt(selectedChoice, 10);
+
+		clearInterval(interval);
+
+		if (trivia.utils.numberOfAnswers < 9) {
+
+			trivia.evalAnswerChosen(choiceAsNumber);
+		 	trivia.utils.questionCounter++;
+		 	trivia.utils.numberOfAnswers++;
 		 	trivia.question();
-		} else if (questionCounter === 9) {
-			trivia.evalAnswer();
-			numberOfAnswers++;
-			trivia.evalScore();
+
+		} else if (trivia.utils.questionCounter === 9) {
+
+			trivia.evalAnswerChosen(choiceAsNumber);
+			trivia.utils.numberOfAnswers++;
+			trivia.evalQuestionCount();
+
 		}
 	},
 
-	evalAnswer: function () {
-		if (choiceAsNumber === this.categories[index].questions[questionCounter].answer) {
-			correctAnswers++;
-			console.log('correct');
-		} else if (selectedChoice) {
-			incorrectAnswers++;
-		} else {
-			unansweredQuestions++;
+	evalAnswerChosen: function (choice) {
+
+		if (choice === this.categories[this.utils.selectedIndex].questions[trivia.utils.questionCounter].answer) {
+
+			this.utils.correctAnswers++;
+
+			console.log(
+				'Correct =>',
+				'Selected Answer Index =>', choice, '--', 
+				'Correct Answer Index =>', trivia.categories[this.utils.selectedIndex].questions[trivia.utils.questionCounter].answer
+			);
+
+		} else if (choice !== this.categories[this.utils.selectedIndex].questions[trivia.utils.questionCounter].answer && choice !== undefined) {
+
+			this.utils.incorrectAnswers++;
+
+			console.log(
+				'Incorrect =>',
+				'Selected Answer Index =>', choice, '--', 
+				'Correct Answer Index =>', trivia.categories[this.utils.selectedIndex].questions[trivia.utils.questionCounter].answer
+			);
+
+		} else if (choice === undefined) {
+
+			this.utils.unansweredQuestions++;
+
+			console.log(
+				'Unanswered =>',
+				'Selected Answer Index =>', choice, '--', 
+				'Correct Answer Index =>', trivia.categories[this.utils.selectedIndex].questions[trivia.utils.questionCounter].answer
+			);
+
 		}
+
 	},
 
-	evalScore: function () {
-		if (numberOfAnswers === 10) {
+	evalQuestionCount: function () {
+
+		if (this.utils.numberOfAnswers === 10) {
+
 			$('.trivia-wrap').empty();
 			$('.time-wrap').remove();
-			trivia.answerResults();
-		}	
+			this.calcResults();
+
+		}
+
 	},
 
-	answerResults: function () {
-		$('.trivia-wrap').append('<h1 class="animated infinite flash">Calculating...</h1>');
-		if (correctAnswers === 10) {
-			$('body').append('<div class="overlay"></div>');
-			console.log('million');
+	calcResults: function () {
+
+		if (this.utils.correctAnswers === 10) {
+			$('.cat-title').remove();
+			$('body').append(
+				'<div class="overlay"></div>' +
+				'<div class="million animated zoomIn text-center">' +
+					'<h1>Congratulations you are a Millionaire!!!</h1>' +
+					'<button id="reset" onClick="trivia.reset();">Play Again</button>' +
+				'</div>'
+			);
+
+		} 
+		else if (this.utils.correctAnswers === 0) {
+
+			$('.trivia-wrap').empty().append(
+				'<button id="reset" onClick="trivia.reset();">Play Again</button>' +
+				'<h4>Correct Answers: ' + trivia.utils.correctAnswers + '</h4>' +
+				'<h4>Incorrect Answers: ' + trivia.utils.incorrectAnswers + '</h4>' +
+				'<h4>Unanswered Questions: ' + trivia.utils.unansweredQuestions + '</h4>' +
+				'<h4>Ummmmmm..... I\'m sorry you missed all of the questions.</h4>'
+			);
+
 		}
-		setTimeout( function () {
-			$('.trivia-wrap').empty();
+		else {
+
 			$('.trivia-wrap').append(
-			'<h4>Correct Answers: ' + correctAnswers + '</h4><h4>Incorrect Answers: ' + incorrectAnswers + '</h4><h4>Unanswered Questions: ' + unansweredQuestions + '</h4><h2 class="money">You won $' + correctAnswers + '00,000!!' + '</h2>'
-			).removeClass('zoomIn').addClass('zoomIn');;
-		}, 2500);
+				'<h1 class="animated infinite flash">Calculating...</h1>'
+			);
+
+			setTimeout( function () {
+
+				$('.trivia-wrap').empty().append(
+					'<button id="reset" onClick="trivia.reset();">Play Again</button>' +
+					'<h4>Correct Answers: ' + trivia.utils.correctAnswers + '</h4>' +
+					'<h4>Incorrect Answers: ' + trivia.utils.incorrectAnswers + '</h4>' +
+					'<h4>Unanswered Questions: ' + trivia.utils.unansweredQuestions + '</h4>' +
+					'<h2 class="money">You won $' + trivia.utils.correctAnswers + '00,000!!' + '</h2>'
+				).removeClass('zoomIn').addClass('zoomIn');
+
+			}, 2500);
+
+		}
 	},
 
 	reset: function () {
-		location.reload();
+
+		this.utils.selectedIndex = 0,
+		this.utils.questionCounter = 0,
+		this.utils.questionNumber = 1,
+		this.utils.correctAnswers = 0,
+		this.utils.incorrectAnswers = 0,
+		this.utils.unansweredQuestions = 0,
+		this.utils.numberOfAnswers = 0,
+
+		$('.trivia-wrap').remove();
+		$('.overlay').remove();
+		$('.million').remove();
+		$('.trivia-body').append(
+			'<div class="trivia-wrap animated zoomIn">' + 
+				'<h2>Please select a category</h2>' +
+				'<button id="Math" class="category">Math</button>' +
+				'<button id="Football" class="category">Football</button>' +
+			'</div>'
+		);
+
+		$('button.category').on('click', function (e) {
+
+			trivia.categoryClick(e);
+
+		});
+
 	}
 };
 
@@ -324,26 +372,51 @@ var trivia = {
 window.onload = function () {
 
 	setTimeout( function () {
+
 		$('.directions').addClass('wobble');
+
 	}, 3500);
 
 
-	$('.directions').click(function(){
+	$('.directions').click( function () {
+
 		$('body').append(
-			'<div class="directions-container animated zoomIn"><div class="container"><span><a href="#" class="close animated pulse infinite">&#10005;</a></span><div class="row align-items-center"><div class="col-12 text-center directions-text"><h1>Directions</h1><h4 class="directions-instructions">' + trivia.directions + '</h4></div></div></div></div>'
+			'<div class="directions-container animated zoomIn">' +
+				'<div class="container">' +
+					'<span><a href="#" class="close animated pulse infinite">&#10005;</a></span>' +
+					'<div class="row align-items-center">' +
+						'<div class="col-12 text-center directions-text">' +
+							'<h1>Directions</h1>' +
+							'<h4 class="directions-instructions">' + trivia.utils.directions + '</h4>' +
+						'</div>' +
+					'</div>' +
+				'</div>' +
+			'</div>'
 		);
 
 
-		$('.close').click(function() {
+		$('.close').click( function () {
+
 			$('.directions').remove();
 			$('.directions-container').removeClass('zoomIn').addClass('zoomOut');
+
 			setTimeout(function() { 
 				$('.directions-container').remove();
-				$('.directions').removeClass('infinite tada').addClass('zoomOut');
-				$('.trivia-body').append('<div class="trivia-wrap animated zoomIn"><h2>Please select a category</h2><button id="Math" class="category">Math</button><button id="Sports" class="category">Sports</button><button id="Computers" class="category">Computers</button></div>');
-				$('button.category').on('click',function(e){
+				$('.directions').removeClass('infinite wobble').addClass('zoomOut');
+				$('.trivia-body').append(
+					'<div class="trivia-wrap animated zoomIn">' + 
+						'<h2>Please select a category</h2>' +
+						'<button id="Math" class="category">Math</button>' +
+						'<button id="Football" class="category">Football</button>' +
+					'</div>'
+				);
+
+				$('button.category').on('click', function (e) {
+
 					trivia.categoryClick(e);
+
 				});
+
 			}, 500);
 
 		});
